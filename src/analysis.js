a0f88b26e08eba5520ecf37fb019279f07682dab
chubbystrings <emeka.okwor@decagon.dev>
2021-04-12 20:02:01 +0100

    initial cmmmit, all tests passing

--- src/analysis.js
+++ src/analysis.js
@@ -1,4 +1,4 @@
-const { getTrips } = require('api');
+const { getTrips, getDriver } = require('api');
 
 /**
  * This function should return the trip data analysis
@@ -6,8 +6,167 @@ const { getTrips } = require('api');
  * Question 3
  * @returns {any} Trip data analysis
  */
+
+// create an array of unique driverIDs to pass in to moreThan one trips function
+ const getRemovedDuplicateDriverIDs = (arr) => {
+
+  let driverIDs = arr.map((trip) => {
+    return trip.driverID
+  });
+
+  const noDuplicatesArr = driverIDs.filter((driver, position) => {
+    return driverIDs.indexOf(driver) == position;
+  });
+  return noDuplicatesArr;
+};
+
+// function to convert invalid bill type to a valid bill amount
+const reduceArrOfNumbers = (arr) => {
+  return arr.map((trip) => new String(trip.billedAmount).replace(/,/g, '')).reduce((acc, cur) => parseFloat(acc) + parseFloat(cur), 0)
+}
+
+
+//function to create bills for analysis function return values
+const totalBilled = (arr) => {
+  const billedTotal = reduceArrOfNumbers(arr)
+  const cashBilledTotal = reduceArrOfNumbers(arr.filter((trip) => trip.isCash))
+  const nonCashBilledTotal = reduceArrOfNumbers(arr.filter((trip) => !trip.isCash))
+  return {
+    billedTotal: Number(billedTotal.toFixed(2)),
+    cashBilledTotal: Number(cashBilledTotal.toFixed(2)),
+    nonCashBilledTotal: Number(nonCashBilledTotal.toFixed(2)),
+  }
+}
+
+// //function to return length of trips for both cash and noncash trips
+const trips = (arr) => {
+  const noOfCashTrips = arr.filter((trip) => trip.isCash).length
+  const noOfNonCashTrips = arr.filter((trip) => !trip.isCash).length
+
+  return {
+    noOfCashTrips,
+    noOfNonCashTrips
+  }
+}
+
+// function to check and return number of drivers with more than one trips
+const moreThanOneTrips = async (arr) => {
+  let numberOfTrips = 0
+  const promises = arr.map(async (driver) => {
+    try {
+      const driverDetails = await getDriver(driver);
+      if (driverDetails) return driverDetails;
+    } catch (error) {}
+  });
+  let allNewDrivers = await Promise.all(promises);
+  allNewDrivers = allNewDrivers.filter(Boolean);
+ 
+  allNewDrivers.forEach((driver) => {
+    if (driver.vehicleID.length > 1) {
+      numberOfTrips++;
+    }
+  });
+  return numberOfTrips;
+};
+
+
+const mostTripsByADriver = async (trips) => {
+  // create an b=object to count occurence of trips for each driver
+  const driversObject = trips.reduce((tally, driver) => {
+    tally[driver.driverID] = (tally[driver.driverID] || 0) + 1;
+    return tally;
+  }, {});
+
+  //find the maximum number of trips
+  const result = Math.max(...Object.values(driversObject));
+
+  // create an array of arrays for drivers
+  const x = Object.entries(driversObject);
+  
+  //filter this array to get drivers with the maximum number of trips
+  const newX = x.filter(driver => driver[1] === result);
+  const allDriverPromise = newX.map(async (driver) => {
+    try {
+      const driverDetails = await getDriver(driver[0]);
+      driverDetails.id = driver[0]
+      if (driverDetails) return driverDetails;
+    } catch (error) {}
+  });
+  const allDriver = await Promise.all(allDriverPromise);
+  const driverTrips = trips.filter((driver) => driver.driverID === allDriver[0].id)
+  const totalAmountEarned = reduceArrOfNumbers(driverTrips)
+  allDriver[0].totalAmountEarned = totalAmountEarned
+  allDriver[0].noOfTrips = result
+  return allDriver[0];
+};
+
+const getHighestEarningDriver = async (trips) => {
+
+  // creates an object to get no of trips of all drivers
+  let driversObject = trips.reduce((tally, driver) => {
+    const bill = Number(new String(driver.billedAmount).replace(/,/g, ''))
+    tally[driver.driverID] = (tally[driver.driverID] || 0) + bill;
+    return tally;
+  }, {});
+
+  const highestEarned = trips.reduce((tally, driver) => {
+    tally[driver.driverID] = (tally[driver.driverID] || 0) + 1;
+    return tally;
+  }, {});
+
+  for (const property in driversObject) {
+    driversObject[property] = Number(driversObject[property].toFixed(2));
+  }
+
+  const maxBill = Math.max(...Object.values(driversObject));
+  const newObjArr = Object.entries(driversObject);
+  const highestEarnerArr = newObjArr.filter((driver) => driver[1] === maxBill);
+  const noOfTrips = highestEarned[highestEarnerArr[0][0]]
+  const driver = await getDriver(highestEarnerArr[0][0]);
+
+  const highestEarningDriver = {
+    name: driver.name,
+    email: driver.email,
+    phone: driver.phone,
+    noOfTrips: noOfTrips,
+    totalAmountEarned: maxBill
+  }
+  return { highestEarningDriver }
+};
+
+
 async function analysis() {
   // Your code goes here
+  const data = await getTrips()
+  let newData = JSON.parse(JSON.stringify(data))
+  let result = {}
+  const { billedTotal, cashBilledTotal, nonCashBilledTotal } = totalBilled(newData)
+  const { noOfCashTrips, noOfNonCashTrips } = trips(newData)
+  const noDuplicatesArr = getRemovedDuplicateDriverIDs(newData)
+  const noOfDriversWithMoreThanOneVehicle = await moreThanOneTrips(noDuplicatesArr)
+  const highestDriver = await mostTripsByADriver(newData)
+  const { highestEarningDriver } = await getHighestEarningDriver(newData)
+
+  const mostTripsByDriver = {
+    name: highestDriver.name,
+    email: highestDriver.email,
+    phone: highestDriver.phone,
+    noOfTrips: highestDriver.noOfTrips,
+    totalAmountEarned: highestDriver.totalAmountEarned
+  }
+  
+  result = {
+    billedTotal,
+    cashBilledTotal,
+    nonCashBilledTotal,
+    noOfCashTrips,
+    noOfNonCashTrips,
+    noOfDriversWithMoreThanOneVehicle,
+    mostTripsByDriver,
+    highestEarningDriver
+  }
+
+  return result
 }
 
 module.exports = analysis;
--- src/report.js
+++ src/report.js
@@ -1,5 +1,5 @@
-const { getTrips } = require('api');
-
+const { getTrips, getDriver, getVehicle } = require('api');
+// const { getAllDrivers }  = require('./analysis');
 /**
  * This function should return the data for drivers in the specified format
  *
@@ -7,8 +7,125 @@ const { getTrips } = require('api');
  *
  * @returns {any} Driver report data
  */
-async function driverReport() {
-  // Your code goes here
+
+// filter driver ids to remove duplicates
+const getAllDriverUniqueIds = (array) => {
+  let driversArray = [];
+  array.forEach((trip) => {
+    driversArray.push(trip.driverID);
+  });
+  const uniqueDrivers = driversArray.filter((driver, index) => {
+    return driversArray.indexOf(driver) == index;
+  });
+  return uniqueDrivers;
+};
+
+// use the unique ids of driver to fetch their details from getDriver and return driver information
+const getDriversDetail = async (driversArr) => {
+  const allDriversPromise = driversArr.map(async (driverID) => {
+    try {
+      const driver = await getDriver(driverID);
+      driver.noOfVehicles = driver.vehicleID.length;
+      driver.vehicleId = driver.vehicleID;
+      driver.id = driverID;
+      if (driver) return driver;
+    } catch (error) {}
+  });
+  let allDrivers = await Promise.all(allDriversPromise);
+  allDrivers = allDrivers.filter(Boolean);
+
+  let result = [];
+  // assign values as requested for tests
+  allDrivers.forEach((driver) => {
+    const newDriver = {
+      name: driver.name,
+      id: driver.id,
+      phone: driver.phone,
+      noOfVehicles: driver.noOfVehicles,
+      vehicleId: driver.vehicleId,
+    };
+    result.push(newDriver);
+  });
+  return result;
+};
+
+// function to change billed amount to number
+const reduceArrOfNumbers = (arr) => {
+    let newValue = arr.map((trip) => new String(trip.billedAmount).replace(/,/g, '')).reduce((acc, cur) => parseFloat(acc) + parseFloat(cur), 0)
+    newValue = Number(toFixed(2))
 }
+
+// function to get all trips for all drivers
+const getNofOfTrips = (driversArray, tripsArray) => {
+
+    //loop through unique ids and filter trips to get individual data for drivers, assign values as requested in arrayOfTrips as a placeHolder
+  const getTrips = driversArray.map((driver) => {
+
+    let arrayOfTrips = tripsArray.filter((trip) => trip.driverID === driver.id);
+    driver.arrayOfTrips = arrayOfTrips;
+
+    driver.noOfTrips = arrayOfTrips.length;
+    driver.noOfCashTrips = arrayOfTrips.filter((trip) => trip.isCash).length;
+    driver.noOfNonCashTrips = arrayOfTrips.filter(
+      (trip) => !trip.isCash
+    ).length;
+
+    driver.totalAmountEarned = reduceArrOfNumbers(arrayOfTrips)
+    driver.totalCashAmount = reduceArrOfNumbers(arrayOfTrips.filter((trip) => trip.isCash))
+    driver.totalNonCashAmount = reduceArrOfNumbers(arrayOfTrips.filter((trip) => !trip.isCash))
+
+    driver.trips = arrayOfTrips.map((trip) => {
+      let obj = {};
+      obj.name = trip.user.name;
+      obj.created = trip.created;
+      obj.pickup = trip.pickup.address;
+      obj.destination = trip.destination.address;
+      obj.billed = Number(new String(trip.billedAmount).replace(/,/g, ''))
+      obj.isCash = trip.isCash;
+      return obj;
+    });
+    return driver;
+  });
+  return getTrips;
+};
+
+// Function to get vehicle details of all drivers
+const getVehicleDetails = async (allDrivers) => {
+
+    // map through all drivers report data get driver ids and call getvehicle function for each driver, add vehicleArr option to data
+  const allNewDRiversPromise = allDrivers.map(async (driver) => {
+    const driverVehicles = driver.vehicleId;
+    const vehicleArrayPromise = driverVehicles.map(async (id) => {
+      const vehicle = await getVehicle(id);
+      return vehicle;
+    });
+    const newVehicles = await Promise.all(vehicleArrayPromise);
+    return { ...driver, vehicleArray: newVehicles };
+  });
+
+  // resolve all promises from map
+  const result = await Promise.all(allNewDRiversPromise);
+
+  //loop through resolved promises of mutated result object and add vehicles property
+  const allNewDrivers = result.map((driver) => {
+    let vehicles = [];
+    driver.vehicleArray.forEach((el) => {
+      vehicles.push({ plate: el.plate, manufacturer: el.manufacturer });
+    });
+    delete driver.arrayOfTrips;
+    delete driver.vehicleArray;
+    return { ...driver, vehicles };
+  });
+  return allNewDrivers;
+};
+
+async function driverReport() {
+    const trips = await getTrips();
+    const driversUniqueIdArray = getAllDriverUniqueIds(trips);
+    const allDriversDetails = await getDriversDetail(driversUniqueIdArray);
+    const driversNoOfTrips = getNofOfTrips(allDriversDetails, trips);
+    const result = await getVehicleDetails(driversNoOfTrips);
+    return result;
+  }
 
 module.exports = driverReport;
